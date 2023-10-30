05264646f7db3c7e9eaa1a93da493cacfd68644c
finalxyws <finalxyws@gmail.com>
2023-10-30 17:08:15 +0800

    Add Class file.

---
+++ class.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+class Person():
+    var_prop = 'test_person'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def eat(self):
+        print('{} is eating.'.format(self.name))
+
+    def sleep(self):
+        print('{} is sleeping.'.format(self.name))
+
+    def play(self):
+        print('{} is playing.'.format(self.name))
+
+    def person_prop(self):
+        print('This is a person class. {0}'.format(self.var_prop))
+
+
+p1 = Person('zhangsan', 20)
+p1.eat()
+p1.sleep()
+p1.play()
+p1.person_prop()
