c396d576fc4c918d071eefb97b9dfa53489cdccb
finalxyws <finalxyws@gmail.com>
2023-10-31 12:19:21 +0800

    Inherit from base class.

--- class.py
+++ class.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-class Person():
-    var_prop = 'test_person'
+class Person(object):
+    var_prop = 'Human in nature.'
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, gender):
         self.name = name
         self.age = age
+        self.gender = gender
+
+    def __del__(self):
+        print('{0} is deleted.'.format(self.name))
+
+    def print_info(self):
+        print('Hello, {0}.'.format(self.name))
 
     def eat(self):
         print('{} is eating.'.format(self.name))
@@ -17,15 +24,30 @@ class Person():
     def play(self):
         print('{} is playing.'.format(self.name))
 
-    def person_prop(self):
-        print('This is a person class. {0}'.format(self.var_prop))
-
     def change_prop(self):
         self.var_prop = input('Please input a new prop: ')
         print('The new prop is: {0}'.format(self.var_prop))
 
 
-p1 = Person('zhangsan', 20)
-p1.eat()
-p1.sleep()
-p1.play()
+class PersonVIP(Person):
+    def print_info(self):
+        print('Hello, VIP {0}.'.format(self.name))
+
+
+class PersonGeneral(Person):
+    def print_info(self):
+        print('Hello, General {0}.'.format(self.name))
+
+
+p1 = Person('zhang san', 20, 'male')
+
+p1.print_info()
+# p1.eat()
+# p1.sleep()
+# p1.play()
+
+pVIP = PersonVIP('Sheldon', 40, 'male')
+pVIP.print_info()
+
+pGeneral = PersonGeneral('Lucy', 30, 'female')
+pGeneral.print_info()
