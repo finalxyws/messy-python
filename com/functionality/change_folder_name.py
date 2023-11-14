2b72f77d79e0e6da9aec3b0fc1864bb164a1ced0
finalxyws <finalxyws@gmail.com>
2023-11-14 17:29:25 +0800

    Update change_folder_name.py

--- com/functionality/change_folder_name.py
+++ com/functionality/change_folder_name.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# list all the folders name under one directory and replace '-' with '_' in the folder name
+# list all the folders and files name under one directory and replace '-' and blank if there is with '_' in the folder name
 import os
+import sys
+import re
 
-path = '/Users/sheldon/kaios/working-directory'
-files = os.listdir(path)
+def change_folder_name(path):
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            if re.search(r'[\s-]', dir):
+                new_dir = dir.replace(' ', '_').replace('-', '_').lower()
 
-for file in files:
-    if os.path.isdir(os.path.join(path, file)):
-        os.rename(os.path.join(path, file), os.path.join(path, file.replace('-', '_')))
-        print(file)
-        print(os.path.join(path, file))
-        print(os.path.join(path, file.replace('-', '_')))
-        print('------------------')
+                os.rename(os.path.join(root, dir), os.path.join(root, new_dir))
+                print('change folder name from %s to %s' % (dir, new_dir))
+        for file in files:
+            if re.search(r'[\s-]', file):
+                new_file = file.replace(' ', '_').replace('-', '_').lower()
+                os.rename(os.path.join(root, file), os.path.join(root, new_file))
+                print('change file name from %s to %s' % (file, new_file))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        change_folder_name(sys.argv[1])
     else:
-        print('Not a folder.')
+        print('Usage: %s path' % sys.argv[0])
+        sys.exit(1)
 
-# Path: change_folder_name.py
+# Path: com/functionality/change_file_name.py
