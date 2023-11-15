edd3437c28d379daac616f20981b486d909c9d56
finalxyws <finalxyws@gmail.com>
2023-11-15 10:50:53 +0800

    Reformat the file change_folder_name.py

--- com/functionality/change_folder_name.py
+++ com/functionality/change_folder_name.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# list all the folders and files name under one directory and replace '-' and blank if there is with '_' in the folder name
+# list all the folders and files name under one directory and replace '-' and blank if there is with '_' in the
+# folder name
 import os
 import sys
 import re
+
 
 def change_folder_name(path):
     for root, dirs, files in os.walk(path):
         for dir in dirs:
             if re.search(r'[\s-]', dir):
-                new_dir = dir.replace(' ', '_').replace('-', '_').lower()
+                new_dir = dir.replace(' ', '_').replace('-', '_')
 
                 os.rename(os.path.join(root, dir), os.path.join(root, new_dir))
                 print('change folder name from %s to %s' % (dir, new_dir))
         for file in files:
             if re.search(r'[\s-]', file):
-                new_file = file.replace(' ', '_').replace('-', '_').lower()
+                new_file = file.replace(' ', '_').replace('-', '_')
+
                 os.rename(os.path.join(root, file), os.path.join(root, new_file))
                 print('change file name from %s to %s' % (file, new_file))
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
